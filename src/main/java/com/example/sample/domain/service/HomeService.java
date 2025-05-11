b2f2169b55bc84a270b2fe2686b757844f462c93
kai993 <huf0618@icloud.com>
2025-05-11 15:26:30 +0900

    戻し

--- src/main/java/com/example/sample/domain/service/HomeService.java
+++ src/main/java/com/example/sample/domain/service/HomeService.java
@@ -1,9 +1,11 @@
 package com.example.sample.domain.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import java.time.LocalTime;
+
 import org.springframework.stereotype.Service;
 
 import com.example.sample.config.AppProperties;
+import com.example.sample.domain.exception.SampleServiceException;
 import com.example.sample.domain.repository.HomeRepository;
 
 @Service
@@ -21,6 +23,12 @@ public class HomeService {
         String hello = repository.getHello();
         String key1 = properties.getKey1();
         String content = hello + " " + key1 + "!";
+
+        // LocalTime now = LocalTime.now();
+        // if (now.getMinute() % 2 == 0) {
+        //     throw new SampleServiceException("Service exception occurred");
+        // }
+
         return content;
     }
 
